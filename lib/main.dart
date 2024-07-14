073f59fc18977d42d0e790e45b61a8d283b971ba
Oak Garelik Grangé <awolftwofive@gmail.com>
2024-07-14 12:10:50 -0400

    Changed ColorScheme

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
+        colorSchemeSeed: Colors.teal[600]
       ),
       home: CounterScreen()
     );
