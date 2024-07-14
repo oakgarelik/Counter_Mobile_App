c9caa821c8f758c05b95670a7f5e3f3ff630dd30
Oak Garelik Grangé <awolftwofive@gmail.com>
2024-07-14 00:35:41 -0400

    centered text

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello World'),
+      home: Center(child: Text('Hello World')),
     );
   }
-  
+
 }
